f5c3ac7e9e3cf0586ca4ba7dbe6371dbbf66dae1
Marat <marat.kuz@gmail.com>
2021-06-01 04:21:47 +0300

    Cleanups

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,8 +4,5 @@ import com.typesafe.config._
 object Main extends App {
   val config = ConfigFactory.load()
 
-  val map: Map[String, Int] = Map("one" -> 1, "two" -> 1)
-  println(map.updated("two", 2))
-
 //  val system = ActorSystem("MainSystem")
 }
